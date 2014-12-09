d378604a858d4c8b3e6f5e84ff2a9f38b1e6bd7e
LeviMooreH <levi@moorehojer.dk>
2014-12-09 13:25:02 +0100

    13:24

---
+++ WebSharp/Compiler/Methods/Window/WindowNavigate_Code.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Web_Sharp
+{
+    class WindowNavigate_Code : BaseCode
+    {
+        public WindowNavigate_Code()
+        {
+            name = "Navigate";
+        }
+
+        public override bool Run()
+        {
+            if (NextToken() == "(")
+            {
+                string _string = NextToken();
+                if (NextToken() == ")" && NextToken() == ";")
+                {
+                    AddCode("location.href = " + _string + ";");
+                }
+                else return false;
+            }
+            else return false;
+
+            return true;
+        }
+    }
+}
---
+++ WebSharp/Compiler/Methods/Window/Window_Code.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Web_Sharp
+{
+    class Window_Code : BaseCode
+    {
+        public Window_Code()
+        {
+            name = "Window";
+            autoComplete = new List<string>() { "Navigate" };
+            highlight = "Window";
+            sub = new List<BaseCode>() {
+                new WindowNavigate_Code()
+            };
+        }
+
+        public override bool Run()
+        {
+            AddCodeOnNewLine("window.");
+            if (NextToken() == ".")
+            {
+                return RunSub(NextToken());
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
